72857e03dc65906f017c389a6b3d0b9c13fad4ca
Nick Grossi <nickg@orca.bi>
2026-01-19 14:23:36 -0500

    Fix ESM import issue with vite plugin using async config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  }
+export default defineConfig(async () => {
+  const reactPlugin = (await import("@vitejs/plugin-react-swc")).default;
+  return {
+    plugins: [reactPlugin()],
+    server: {
+      port: 5173
+    }
+  };
 });
 
 
